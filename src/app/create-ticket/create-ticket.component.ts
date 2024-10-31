cf2930917b1353369f6becf76998b6e0f0e715b8
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-10-31 14:40:40 +0100

    ✨ feat(app): new features : create ticket

--- src/app/create-ticket/create-ticket.component.ts
+++ src/app/create-ticket/create-ticket.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-ticket',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-ticket.component.html',
-  styleUrl: './create-ticket.component.css'
+  styleUrl: './create-ticket.component.css',
 })
 export class CreateTicketComponent {
+  http = inject(HttpClient);
 
+  ticketData = {
+    created_by: '',
+    description: '',
+    priorite: '',
+    // assigne: '',
+  };
+
+  createTicket() {
+    this.http
+      .post('http://localhost:3000/createticket', this.ticketData)
+      .subscribe(() => {
+        this.ticketData = {
+          created_by: '',
+          description: '',
+          priorite: '',
+          // assigne: '',
+        };
+      });
+  }
 }
