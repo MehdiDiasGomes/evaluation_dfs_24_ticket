fbbc273967ac24a1e881adf06bba39229a0b546a
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-10-31 13:56:51 +0100

    ✨ feat(app): add list of tickets

--- src/app/list-of-ticket/list-of-ticket.component.ts
+++ src/app/list-of-ticket/list-of-ticket.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list-of-ticket',
   standalone: true,
   imports: [],
   templateUrl: './list-of-ticket.component.html',
-  styleUrl: './list-of-ticket.component.css'
+  styleUrl: './list-of-ticket.component.css',
 })
 export class ListOfTicketComponent {
-  ticketsList = [
-    { id: 1, title: 'Ticket 1', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
-    { id: 2, title: 'Ticket 2', description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
-    { id: 3, title: 'Ticket 3', description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' }
-  ]
+  ticketsList: any = [];
+  http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http
+      .get('http://localhost:3000/listoftickets')
+      .subscribe((data: any) => {
+        this.ticketsList = data;
+        console.log(this.ticketsList);
+      });
+  }
 }
